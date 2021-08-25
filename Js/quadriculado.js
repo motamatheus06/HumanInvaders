0243ca78f84de4cb28a9bea31b441637ae88b273
motamatheus06 <motamatheus06@gmail.com>
2021-08-24 23:51:11 -0300

    Add emojis and change the field

--- Js/quadriculado.js
+++ Js/quadriculado.js
@@ -1,10 +1,23 @@
-var i = 0;
+const bolas = [];
 
-while (i < 255) {
-  i++;
-  const div1 = document.querySelector(".campo");
-  const squaresdiv = document.createElement("div");
-  squaresdiv.classList.add("quadradinhos");
-  div1.insertAdjacentElement("beforeend", squaresdiv);
+for (let y=0; y<51; y++) {
+  for (let x=0; x<51; x++) {
+    const square = document.createElement("div");
+    square.classList.add("quadradinho");
+    campo.appendChild(square);
+    square.x = x;
+    square.y = y;
+    square.onclick = squareClick;
+  }
+
+function squareClick(ev) {
+  console.log(this.x, this.y);
+  const bola = document.createElement("b");
+  this.appendChild(bola);
+  bola.innerText = "👽";
+  bola.x = this.x;
+  bola.y = this.y;
+  bolas.push(bola);
+}
 }
 
